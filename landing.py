7b43978a60036e85e1de58fd75a55e740f6acbe8
Kimmo <kkpsiren@gmail.com>
2022-06-11 11:17:39 +0300

    cleaned

--- landing.py
+++ landing.py
@@ -11,12 +11,19 @@ def get_groups(data,groups):
 
 def landing_page(df,df_minted,df_images):
     st.image('https://openseauserdata.com/files/e22c98856cf40d4efb9d2dcb69d25c9b.png')
-    st.write("GodMode by FlipsideCrypto")
+    st.markdown("""## Assets Removed from Osmosis 
+supported by GodMode by FlipsideCrypto""")
     st.markdown(
         """
 On June 8th, a critical bug was found on Osmosis that led to the theft of several million dollars from liquidity pools. In an effort to help provide the Osmosis team with important data, we’ve curated these flash bounties to surface metrics that the team has requested. The upgrade that contained the exploited bug occurred at block height 4707300, and the chain was halted at block 4713064.
 
-What was the total dollar amount of assets that was improperly removed from pools due to the bug, and what is the list of addresses that had joined a pool since the upgrade? What is the dollar value deposited of those addresses joining pools? What is the cumulative value of funds that joined and exited?
+What was the total dollar amount of assets that was improperly removed from pools due to the bug, 
+
+and what is the list of addresses that had joined a pool since the upgrade? 
+
+What is the dollar value deposited of those addresses joining pools? 
+
+What is the cumulative value of funds that joined and exited?
 
 Hints: addresses that run the MsgJoinPool & MsgExitPool message types
         """ 
--- queries.py
+++ queries.py
@@ -1,29 +1,24 @@
-IMAGE_QUERY = """
-with min_dt as (
-  select min(block_timestamp) + interval '1 hour' as min_timestamp
-  from ethereum.core.ez_nft_mints
-  where nft_address = '0x903e2f5d42ee23156d548dd46bb84b7873789e44' 
+MSG_QUERY = """
+with tx_data as (
+select tx_id,msg_type
+from osmosis.core.fact_msgs
+  where block_id >= 4707300
+  and block_id <= 4713064
+  and (msg_type = 'pool_joined'
+  or msg_type = 'pool_exited')
 ),
-nft_data as (
-select NFT_TO_ADDRESS, tokenid, block_timestamp
-from ethereum.core.ez_nft_mints
-  where nft_address = '0x903e2f5d42ee23156d548dd46bb84b7873789e44'
-  and block_timestamp <= (select min_timestamp from min_dt) 
-order by 1 desc
-  ),
-nft_per_wallet as (
-  select nft_to_address, count(tokenid)
-  from nft_data
-  group by 1
+ready_data as (
+select block_id, block_timestamp, tx_from, tx_status, t.msg_type
+from osmosis.core.fact_transactions f
+inner join tx_data t on t.tx_id=f.tx_id
+  where block_id >= 4707300
+  and block_id <= 4713064
 )
-select buyer_address, nft_address, tokenid, price, m.token_metadata, m.token_metadata_uri, m.token_name, m.project_name, image_url
-from ethereum.core.ez_nft_sales n
-left join flipside_prod_db.ethereum.nft_metadata m on m.contract_address = nft_address and token_id=tokenid
-where buyer_address in (select nft_to_address from nft_per_wallet)
-and block_timestamp > getdate() - interval '6 months'
-and  nft_address != '0x903e2f5d42ee23156d548dd46bb84b7873789e44'
-and currency_symbol in ('WETH','ETH')
-and price > 0
+select tx_from, msg_type, count(tx_status)
+from ready_data
+  where tx_status = 'SUCCEEDED'
+  group by 1,2
+order by 3 desc
 """
 
 SQL_QUERY = """
